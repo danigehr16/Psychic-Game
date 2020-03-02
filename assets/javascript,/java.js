44f93920f53b37bc32c229af9444303bcdc80d04
dani <gehr.danielle@gmail.com>
2020-03-02 16:51:27 -0500

    real var for js

--- assets/javascript,/java.js
+++
@@ -1,8 +0,0 @@
-var alphabet = "abcdefghijklmnopqrstuvwxyz"
-
-var wins = 0;
-var losses = 0;
-var guesses = 9;
-var guessesRemain = 9;
-var guessedLetters = [];
-
---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+var alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guessesRemain = 9;
+var guessedLetters = [];
